c6040727e78e5a58a4e286ef3aacd0f69bb39bc8
Mohammad Syahid Wildan <mohammad.syahid.wildan@gmail.com>
2015-10-07 06:34:49 +0000

    Latihan 4 Prettification

---
+++ lists/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from lists.models import Item, List
+
+def home_page(request):
+    return render(request, 'home.html')
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
+    counter = items.count()
+    status = "oh tidak"
+    if counter == 0:
+       status = "yey, waktunya berlibur"
+    elif counter < 5:
+       status = "sibuk tapi santai"
+    else:
+       status = "oh tidak"
+
+    return render(request, 'list.html', {'items': items, 'status':status, 'list': list_})
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/%d/' % (list_.id,))
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/%d/' % (list_.id,))
